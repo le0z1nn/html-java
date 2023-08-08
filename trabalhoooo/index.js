7a3d420918b3476a89301d2798d05b19f79120a1
ozin <133661986+le0z1nn@users.noreply.github.com>
2023-08-08 14:13:06 -0300

    Add files via upload

---
+++ trabalhoooo/index.js
@@ -0,0 +1,14 @@
+function mostrarMenu() {
+    var menu = document.getElementById("nav-header");
+    var icone = document.getElementById("icone");
+
+    if(getComputedStyle (menu).display == 'none'){
+        menu.style.display = "flex";
+        icone.classList.remove("fa-bars");
+        icone.classList.add("fa-times");
+    } else{
+        menu.style.display = "none";
+        icone.classList.remove("fa-times");
+        icone.classList.add("fa-bars");
+    }
+}
